d4650398cfda0ebe562ddc585d1fc574f101d003
gelmen <danil.danich.22825@gmail.com>
2025-02-21 14:05:25 +0300

    step 8 done

---
+++ bin/brain-progression.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import {hello, getRandonInt} from '../src/cli.js';
+
+
+
+const makeProgress = () => {
+    const masProg = [];
+    let countEllOfProg = getRandonInt(15);
+    if (countEllOfProg < 5) {
+        countEllOfProg = 5;
+    }
+    let progDifference = getRandonInt(10);
+    if (progDifference === 0) {
+        progDifference = 1;
+    }
+
+    let startOfProgression = getRandonInt(100);
+    const correctAnswerIndex = getRandonInt(countEllOfProg - 1);
+    let correctAnswer;
+    for (let i = 0; i < countEllOfProg; i++) {
+        if (i === correctAnswerIndex) {
+            masProg.push("..");
+            correctAnswer = startOfProgression;
+            startOfProgression += progDifference;
+        }
+        masProg.push(startOfProgression);
+        startOfProgression += progDifference;
+    }
+    const stringProg = masProg.join(' ')
+    return [stringProg, correctAnswer];
+}
+
+
+
+
+const gameProg = () => {
+    const userName = hello();
+    console.log('What number is missing in the progression?')
+    let count = 0;
+    while (count != 3) {
+        const [stringProg, correctAnswer] = makeProgress();
+        console.log(`Question: ${stringProg}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        if (+userAnswer === correctAnswer){
+            count++
+            console.log("Correct!");
+        } else {
+            console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
+            break;
+        }
+    }
+    if (count === 3) {
+        console.log(`Congratulations, ${userName}!`)
+    }
+}
+
+gameProg();
